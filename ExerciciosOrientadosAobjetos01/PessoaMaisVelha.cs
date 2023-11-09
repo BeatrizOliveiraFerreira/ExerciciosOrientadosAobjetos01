9fde91d4400334438bf2702d1512f334f648f6b7
Beatriz Oliveira Ferreira <beaferreira.trabalho@gmail.com>
2023-11-08 22:04:25 -0300

    Add project files.

---
+++ ExerciciosOrientadosAobjetos01/PessoaMaisVelha.cs
@@ -0,0 +1,9 @@
+﻿
+namespace ExerciciosOrientadosAobjetos01 {
+    internal class PessoaMaisVelha {
+        public string nome1;
+        public string nome2;
+        public int idade1;
+        public int idade2;
+    }
+}
---
+++ ExerciciosOrientadosAobjetos01/Program.cs
@@ -0,0 +1,27 @@
+﻿using ExerciciosOrientadosAobjetos01;
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program {
+        static void Main(string[] args) {
+            PessoaMaisVelha a, b;
+
+            a = new PessoaMaisVelha();
+            b = new PessoaMaisVelha();
+
+            a.nome1 = Console.ReadLine();
+            b.idade1 = int.Parse(Console.ReadLine());
+            a.nome2 = Console.ReadLine();
+            b.idade2 = int.Parse(Console.ReadLine());
+
+            if (b.idade1 > b.idade2 ) {
+                Console.WriteLine($"Pessoa mais velha: {a.nome1}");
+            }
+            else {
+                Console.WriteLine($"Pessoa mais velha: {a.nome2}");
+            }
+
+        }
+    }
+}
